f5ee749d639a0bab0c320b5e627ab8c221580d8c
Luis Saavedra <luisdavidsag@gmail.com>
2025-02-19 23:17:40 -0500

    advance on authorization

--- py_client/create.py
+++ py_client/create.py
@@ -3,5 +3,12 @@ import requests
 endpoint = "http://127.0.0.1:8000/api/products/"
 
 
-get_response = requests.post(endpoint)  # API
+get_response = requests.post(
+    endpoint,
+    json={
+        "title": "im hereamsdmaskke",
+        "content": "test4",
+        "price": "1.22",
+    },
+)  # API
 print(get_response.json())
